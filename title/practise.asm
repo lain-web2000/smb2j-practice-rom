ec375551ef25ddfc94ce763d60cb136f3d401637
Lain <51301652+lain-web2000@users.noreply.github.com>
2024-12-24 02:52:11 +0000

    Minor bugfix to axe frame numbers. sowwy

--- title/practise.asm
+++ title/practise.asm
@@ -151,6 +151,7 @@ PractiseEnterStage:
     sta FrameCounter                             ;
 	jmp @QuickResume
 @DashOne:
+	inc Hidden1UpFlag							 ; set the hidden 1up flag to true
 	lda FrameCounter                             ; we need to offset the frame counter differently for dash one levels
     sbc #5                                       ;
     sta FrameCounter                             ;
@@ -385,9 +386,6 @@ RedrawLowFreqStatusbar:
     jsr @PrintRule                               ; draw the current framerule value
 	lda StarFlagTaskControl						 ; is the timer counting down at the end of a normal level?
 	cmp #$02									 ; if yes, do not draw frame counter numbers, the vram buffer will cry.
-	beq @NoFC									 ;
-	lda OperMode								 ; is the timer counting down at the end of a castle level?
-	cmp #VictoryMode							 ; if yes, do not draw frame counter numbers, the vram buffer will not cry as much, but still.
 	beq @NoFC									 ;
     jsr @PrintFramecounter                       ; draw the current framecounter value
 @NoFC:
